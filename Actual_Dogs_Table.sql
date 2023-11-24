db8e8c8e8ca5c1a7c0a32e680857564c81fd32be
jsaha92 <33520333+jsaha92@users.noreply.github.com>
2023-11-24 12:28:56 -0500

    Update Actual_Dogs_Table.sql
    Changed a few tables

--- Actual_Dogs_Table.sql
+++ Actual_Dogs_Table.sql
@@ -1,28 +1,28 @@
 CREATE TABLE Shelters (
     Shelter_ID INT PRIMARY KEY,
-    Shelter VARCHAR(255) NOT NULL,
-    Location VARCHAR(255) NOT NULL,
-    Website VARCHAR(255) NOT NULL
+    Shelter VARCHAR(50),
+    Location VARCHAR(25),
+    Website VARCHAR(255) 
 );
 
 CREATE TABLE Vets (
     Vet_ID INT PRIMARY KEY,
-    Vet_Name VARCHAR(255) NOT NULL,
+    Vet_Name VARCHAR(75) NOT NULL,
     Address VARCHAR(255) ,
-    Phone VARCHAR(20) ,
+    Phone VARCHAR(11) ,
     Website VARCHAR(255) 
 );
 
 
 CREATE TABLE Pets (
     Pet_ID INT PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    Breed VARCHAR(255),
-    Color VARCHAR(255),
+    Name VARCHAR(20) NOT NULL,
+    Breed VARCHAR(75),
+    Color VARCHAR(50),
     Age VARCHAR(10),
-    Size VARCHAR(50),
-    Gender VARCHAR(10),
-    Weight DECIMAL(5,2),
+    Size VARCHAR(20),
+    Gender VARCHAR(3),
+    Weight INT,
     Shelter_ID INT,
     FOREIGN KEY (Shelter_ID) REFERENCES Shelters(Shelter_ID)
 );
@@ -39,37 +39,37 @@ CREATE TABLE Compatibility (
 
 CREATE TABLE Owners (
     owner_ID INT PRIMARY KEY,
-    First_Name VARCHAR(255) NOT NULL,
-    Last_Name VARCHAR(255) NOT NULL,
+    First_Name VARCHAR(30) NOT NULL,
+    Last_Name VARCHAR(30) NOT NULL,
     Age INT,
     Had_Previous_Dog BOOLEAN,
-    Home_Size VARCHAR(50),
+    Home_Size VARCHAR(10),
     Nearest_Shelter INT,
     FOREIGN KEY (Nearest_Shelter) REFERENCES Shelters(Shelter_ID)
 );
 
 CREATE TABLE Adoption (
-    Adoption_ID INT PRIMARY KEY,
-    Pet_ID INT,
+    Pet_ID INT PRIMARY KEY,
     Owner_ID INT,  
-    Adoption_Fee DECIMAL(8,2),
+    Adoption_Fee INT,
     FOREIGN KEY (Pet_ID) REFERENCES Pets(Pet_ID),
     FOREIGN KEY (Owner_ID) REFERENCES Owners(owner_ID)
 );
 
+
 CREATE TABLE OwnerContact (
-    ContactID INT PRIMARY KEY,
-    owner_ID INT,
-    Phone_Number VARCHAR(20) NOT NULL,
+    owner_ID INT PRIMARY KEY,
+    Phone_Number VARCHAR(10) NOT NULL,
     Email VARCHAR(255),
     FOREIGN KEY (owner_ID) REFERENCES Owners(owner_ID)
 );
 
+
 CREATE TABLE OwnerPreferences (
     owner_ID INT PRIMARY KEY,
-    Preferred_Breed VARCHAR(255),
-    Preferred_Size VARCHAR(50),
-    Preferred_Gender VARCHAR(10),
+    Preferred_Breed VARCHAR(75),
+    Preferred_Size VARCHAR(20),
+    Preferred_Gender VARCHAR(3),
     Max_Age INT,
     Vaccination_Required BOOLEAN,
     Good_with_Kids_Required BOOLEAN,
